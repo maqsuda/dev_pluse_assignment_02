0dad9deb15fbcf299a984f6826cda3f078c75629
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-06-07 18:06:15 +0600

    added register

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, {
 } from "express";
 import { pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 
 const app: Application = express();
 app.use(express.json());
@@ -19,5 +20,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
 
 export default app;
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,16 +1,21 @@
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
+import bcrypt from "bcryptjs";
 
 const createUserToDB = async (payLoad: IUser) => {
   const { name, email, password, role } = payLoad;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  // console.log(hashPassword);
   const result = await pool.query(
     `
         INSERT INTO users(name, email, password, role)
         VALUES($1,$2,$3,$4)
         RETURNING *
     `,
-    [name, email, password, role],
+    [name, email, hashPassword, role],
   );
+  delete result.rows[0].password;
   return result;
 };
 
